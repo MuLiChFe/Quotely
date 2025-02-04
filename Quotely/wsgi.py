b3c2a685d458bb849886e02499b5825726388d81
Li Zefeng <76549977+MuLiChFe@users.noreply.github.com>
2025-02-04 14:40:14 +0800

    Update wsgi.py

--- Quotely/wsgi.py
+++ Quotely/wsgi.py
@@ -13,5 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quotely.settings')
 
-application = get_wsgi_application()
 app = get_wsgi_application()
